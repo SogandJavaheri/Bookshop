897f99f9ed2ce7cd2d837d696e48be63adce7372
Sogand Javaheri <javaherisogand78@gmail.com>
2025-01-22 22:05:24 +0330

    implement Bookshop class

---
+++ src/com/github/sogandjavaheri/Bookshop/Bookshop.java
@@ -0,0 +1,11 @@
+package com.github.sogandjavaheri.Bookshop;
+
+public class Bookshop {
+
+    String book_name;
+    String author;
+    int pages;
+    int year_of_publish;
+    String category;
+
+}
